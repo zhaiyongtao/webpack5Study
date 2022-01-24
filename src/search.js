c0025e7df98d424b1530370ed20af8b96a6d6391
zhaiyongtao <501671900@qq.com>
2022-01-25 01:28:17 +0800

    fix： 文件 file loader url loader

--- src/search.js
+++ src/search.js
@@ -1,15 +1,19 @@
-import React from "react";
-import  ReactDOM from "react-dom";
-import './search.css'
-import './searchLess.less'
 
-class Search extends  React.Component {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './assets/images/bg.jpg';
+import './search.less';
+
+class Search extends React.Component {
+
     render() {
-        return <div className="search searchLess">
-            search
-        </div>
+        return <div className="search-text">
+            搜索文字的内容<img src={ logo } />
+        </div>;
     }
 }
 
-
-ReactDOM.render(<Search/>, document.getElementById('root'))
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+);
